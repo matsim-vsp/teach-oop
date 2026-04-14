6b80dd8e788dc479e90c347fbd9f161b4aefbf8b
Kai Nagel <nagel@vsp.tu-berlin.de>
2026-04-14 12:35:01 +0200

    committing from last year

--- src/main/java/nn_junit/UtilsTest.java
+++ src/main/java/nn_junit/UtilsTest.java
@@ -1,8 +1,8 @@
 package nn_junit;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.Before;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class UtilsTest{
 
@@ -10,8 +10,7 @@ public class UtilsTest{
 	public void testMyMax() {
 		double expected = 7. ;
 		double actual = Utils.myMax( 3., 5., 7.  ) ;
-		double epsilon = 0. ;
-		Assert.assertEquals( expected, actual, epsilon );
+		Assertions.assertEquals( expected, actual );
 
 	}
 
@@ -20,21 +19,21 @@ public class UtilsTest{
 		double expected = 5. ;
 		double actual = Utils.myMax( -3., 5., -7.  ) ;
 		double epsilon = 0. ;
-		Assert.assertEquals( expected, actual, epsilon );
+		Assertions.assertEquals( expected, actual, epsilon );
 	}
 	@Test
 	public void testMyMax3() {
 		double expected = -3. ;
 		double actual = Utils.myMax( -3., -5., -7.  ) ;
 		double epsilon = 0. ;
-		Assert.assertEquals( expected, actual, epsilon );
+		Assertions.assertEquals( expected, actual, epsilon );
 	}
 	@Test
 	public void testMyMax4() {
 		double expected = Double.POSITIVE_INFINITY;
 		double actual = Utils.myMax( Double.MAX_VALUE, Double.MIN_VALUE, Double.POSITIVE_INFINITY  ) ;
 		double epsilon = 0. ;
-		Assert.assertEquals( expected, actual, epsilon );
+		Assertions.assertEquals( expected, actual, epsilon );
 	}
 
 }
